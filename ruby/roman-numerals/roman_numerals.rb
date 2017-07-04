50323f9acd095f43f45e88bb5065821dd1defbe7
Joe <joseph_strauss@yahoo.com>
2017-07-04 10:45:21 -0400

    Roman Numerals...

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,4 +1,18 @@
-class RomanNumerals
+require 'pry'
+class Fixnum
   def to_roman
+    raise 'Only works on numbers between 1 and 3888!' if self <= 0 || self > 3888
+
+    ints = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    romNums = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+    string = ''
+    remainder = self
+
+    ints.zip(romNums).each do |int, romNum|
+      quotient = remainder / int
+      string << romNum * quotient
+      remainder = remainder % int
+    end
+    string
   end
 end
